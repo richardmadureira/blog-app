090996f77f4e607dda3a387bb17bf870c202b9c1
Richard Mendes Madureira <richard.madureira@dataprev.gov.br>
2021-02-17 17:55:37 -0300

    criação de componente Post

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import Post from '../components/Post';
 
 export default function Home({ allPostsData }) {
   return (
@@ -13,15 +14,8 @@ export default function Home({ allPostsData }) {
         <section className="container">
         <h2 className="text-primary">Posts</h2>
           <div className="d-flex flex-wrap">
-            {allPostsData.map(({ id, title, publishDate }) => (
-              <Link key={id} href={`/posts/${id}`}>
-                <div className="card m-1" role="button">
-                  <div className="card-body">
-                    <div className="card-title text-primary">{title}</div>
-                    <div className="card-subtitle">{publishDate}</div>
-                  </div>
-                </div>
-              </Link>
+            {allPostsData.map(post => (
+              <Post key={post.id} post={post}/>
             ))}
           </div>
         </section>
